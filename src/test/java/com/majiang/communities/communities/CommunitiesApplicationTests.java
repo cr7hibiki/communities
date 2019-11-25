ac2c1e6107773ed25bc38bf1a39c252166f2e0f9
cr7hibiki <you@example.com>
2019-11-25 22:17:53 +0800

    init repo

---
+++ src/test/java/com/majiang/communities/communities/CommunitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majiang.communities.communities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunitiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
