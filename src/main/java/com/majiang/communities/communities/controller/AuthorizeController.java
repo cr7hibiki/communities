3483ef74272b90d35a3b3b0344ab57b846933146
cr7hibiki <you@example.com>
2019-12-07 12:40:56 +0800

    new commit

--- src/main/java/com/majiang/communities/communities/controller/AuthorizeController.java
+++ src/main/java/com/majiang/communities/communities/controller/AuthorizeController.java
@@ -3,12 +3,14 @@ package com.majiang.communities.communities.controller;
 import com.majiang.communities.communities.dto.AccessTokenDTO;
 import com.majiang.communities.communities.dto.GithubUser;
 import com.majiang.communities.communities.provider.GithubProvider;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.security.Provider;
 
 @Controller
@@ -23,7 +25,8 @@ public class AuthorizeController {
     private String redirectUri;
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,
-                           @RequestParam(name="state")String state){
+                           @RequestParam(name="state")String state,
+                           HttpServletRequest request){
         final AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -33,6 +36,12 @@ public class AuthorizeController {
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
         System.out.println(user.getName());
+        if(user!=null){
+            //登录成功，写cookie和session
+            request.getSession().setAttribute("user",user);
+        }else {
+            //登录失败，重新登录
+        }
         return "index";
     }
 }
